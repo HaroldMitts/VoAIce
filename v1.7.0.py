27ba6857049792ad52d69998110f7d61d39d3062
Harold Mitts <HaroldMitts@users.noreply.github.com>
2023-04-18 21:35:54 -1000

    removed mp3 functionality

---
+++ v1.7.0.py
@@ -0,0 +1,108 @@
+import json
+import os
+import subprocess
+import tempfile
+import time
+import azure.cognitiveservices.speech as speechsdk
+import openai
+
+def load_api_keys(file_path):
+    with open(file_path, "r") as f:
+        keys = json.load(f)
+    return keys
+
+def transcribe_audio(speech_config):
+    audio_config = speechsdk.AudioConfig(use_default_microphone=True)
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+
+    result = speech_recognizer.recognize_once_async().get()
+    return result.text.strip()
+
+def generate_response(input_text, conversation_history):
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+    ]
+
+    messages.extend(conversation_history)
+    messages.append({"role": "user", "content": input_text})
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        max_tokens=60,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+
+    assistant_response = response['choices'][0]['message']['content']
+    return assistant_response
+
+def synthesize_and_save_speech(speech_config, response_text, file_path):
+    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+    result = speech_synthesizer.speak_text_async(response_text).get()
+
+    with open(file_path, "wb") as f:
+        f.write(result.audio_data)
+
+
+def play_audio(audio_file_path):
+    subprocess.call(["ffplay", "-nodisp", "-autoexit", audio_file_path], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+
+def remove_temp_files(file_path):
+    os.remove(file_path)
+
+def main(quit_phrases=["I quit", "quit", "goodbye", "stop", "exit"]):
+    keys = load_api_keys("C:\\GitHub\\VoAI\\VoAI\\keys.json")
+    azure_api_key = keys["azure_api_key"]
+    azure_region = keys["azure_region"]
+    openai_api_key = keys["openai_api_key"]
+
+    speech_config = speechsdk.SpeechConfig(subscription=azure_api_key, region=azure_region)
+    openai.api_key = openai_api_key
+
+    conversation_history = []
+
+    while True:
+        print("Listening...")
+
+        input_text = transcribe_audio(speech_config)
+        print(f"Input: {input_text}")
+
+        if any(phrase.lower() in input_text.lower() for phrase in quit_phrases):
+            break
+
+        response_text = generate_response(input_text, conversation_history)
+        print(f"Response: {response_text}")
+
+        conversation_history.append({"role": "user", "content": input_text})
+        conversation_history.append({"role": "assistant", "content": response_text})
+
+        with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as f:
+            audio_file_path = f.name
+
+        try:
+            synthesize_and_save_speech(speech_config, response_text, audio_file_path)
+            print(f"Saved audio file: {audio_file_path}")
+            print(f"Number of audio files in directory: {len(os.listdir(os.path.dirname(audio_file_path)))}")
+        except Exception as e:
+            print(f"Error: Failed to synthesize speech - {e}")
+
+        # Play the WAV file
+        try:
+            print("Playing audio...")
+            #play_audio(audio_file_path)
+            print("Audio playback complete.")
+        except Exception as e:
+            print(f"Error: Failed to play WAV - {e}")
+
+        # Remove the temporary files
+        try:
+            remove_temp_files(audio_file_path)
+        except Exception as e:
+            print(f"Error: Failed to remove temporary files - {e}")
+
+        time.sleep(1)  # Add a 1-second delay before the loop starts listening again
+
+if __name__ == "__main__":
+    main(quit_phrases=["I quit", "stop", "exit"])
